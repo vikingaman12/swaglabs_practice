ea65fed5338ea4bdc6bd59befa52fd58ec8a2f28
AmanAshutosh <amanashutosh2013@gmail.com>
2023-04-04 12:36:24 +0530

    more changes

--- simpleFramework/src/test/java/practice1/AddToCartAndOrder.java
+++ simpleFramework/src/test/java/practice1/AddToCartAndOrder.java
@@ -7,9 +7,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import genericUtility.ExcelFile;
 import genericUtility.PropertyFile;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import pom.AddtoCart;
 import pom.CheckoutPage;
 import pom.LoginPage;
@@ -18,26 +21,60 @@ import pom.LoginPage;
 
 public class AddToCartAndOrder {
 
+	//public static WebDriver driver;
 	public static void main(String[] args) throws Throwable {
-		String key = "webdriver.chrome.driver";
-		String value = "./src/main/resources/chromedriver.exe";
-		System.setProperty(key, value);
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		WebDriver driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//		String key = "webdriver.chrome.driver";
+//		String value = "./src/main/resources/chromedriver.exe";
+//		System.setProperty(key, value);
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--remote-allow-origins=*");
+//		WebDriver driver = new ChromeDriver(options);
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
 		/*
 		 * To fetch common data from property file
 		 */
+		
 		PropertyFile plib = new PropertyFile();
 		ExcelFile ef = new ExcelFile();
-		LoginPage login = new LoginPage(driver);
+		
+		/*
+		 * With this you can open any browser, you just need to change the
+		 * browser name in commonData.properties
+		 */
+		String BROWSER = plib.getPropertyKeyValue("browser");
+		WebDriver driver;
+		if(BROWSER.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			//WebDriver driver = new ChromeDriver(options);
+			driver = new ChromeDriver(options);
+		}
+		else if(BROWSER.equalsIgnoreCase("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		else if(BROWSER.equalsIgnoreCase("edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		else
+		{
+			driver = new ChromeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
 		String URL = plib.getPropertyKeyValue("url");
 		String UN = plib.getPropertyKeyValue("username");
 		String PWD = plib.getPropertyKeyValue("password");
 		driver.get(URL);
+		LoginPage login = new LoginPage(driver);
 		login.loginToApp(UN, PWD);
 		
 		driver.getTitle();
@@ -70,7 +107,7 @@ public class AddToCartAndOrder {
 		driver.findElement(By.id("back-to-products")).click();
 		driver.navigate().to("https://www.saucedemo.com/");
 		Thread.sleep(1000);
-		//driver.close();
+		driver.close();
 
 	}
 
--- simpleFramework/src/test/java/scripts/TestCase1.java
+++ simpleFramework/src/test/java/scripts/TestCase1.java
@@ -6,9 +6,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import genericUtility.ExcelFile;
 import genericUtility.PropertyFile;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import pom.AddtoCart;
 import pom.CheckoutPage;
 import pom.LoginPage;
@@ -16,25 +19,48 @@ import pom.LoginPage;
 public class TestCase1 {
 	
 	public static void main(String[] args) throws Throwable {
-		String key = "webdriver.chrome.driver";
-		String value = "./src/main/resources/chromedriver.exe";
-		System.setProperty(key, value);
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		WebDriver driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
 		/*
-		 * To fetch common data from property file
+		 * To fetch common data from property file,Excel File
 		 */
 		PropertyFile plib = new PropertyFile();
 		ExcelFile ef = new ExcelFile();
-		LoginPage login = new LoginPage(driver);
+		
+		/*
+		 *(33-55) With this you can open any browser, you just need to change the
+		 * browser name in commonData.properties
+		 */
+		String BROWSER = plib.getPropertyKeyValue("browser");
+		WebDriver driver;
+		if(BROWSER.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			driver = new ChromeDriver(options);
+		}
+		else if(BROWSER.equalsIgnoreCase("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		else if(BROWSER.equalsIgnoreCase("edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		else
+		{
+			driver = new ChromeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
 		String URL = plib.getPropertyKeyValue("url");
 		String UN = plib.getPropertyKeyValue("username");
 		String PWD = plib.getPropertyKeyValue("password");
 		driver.get(URL);
+		LoginPage login = new LoginPage(driver);
 		login.loginToApp(UN, PWD);
 		
 		AddtoCart ad = new AddtoCart(driver);
